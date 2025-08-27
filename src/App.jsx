9b9636e8ef65ea6a6555fdf663f8e1f38d934303
manikantareddy268 <manikantareddy130263@gmail.com>
2025-08-28 03:04:06 +0530

    rendering array of jokes with for of loop

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,36 @@ function App() {
 
   const jokes =[
     {
+      id: 1,
       joke: "I'm reading a book about anti-gravity. It's impossible to put down!",
       rating: 1  
     },
     {
+      id: 2,
       joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
       rating: 4
     },
     {
+      id: 3,
       joke: "Why don't scientists trust atoms? Because they make up everything!",
       rating: 5
     },
     {
+      id: 4,
       joke: "Why did the bicycle fall over? Because it was two-tired!",
       rating: 4
     }
   ]
 
-  const jokesComponents = [
-    <Joke joke={jokes[0].joke} rating={jokes[0].rating} />,
-    <Joke joke={jokes[1].joke} rating={jokes[1].rating} />
-  ]
+  const jokesComponents = []
+
+  for (const joke of jokes) {
+    jokesComponents.push(<Joke key={joke.id} joke={joke.joke} rating={joke.rating} />)
+  }
 
   return (
       <div>
         <h1>Dad Jokes</h1>
-
-        {/* <Joke joke={jokes[0].joke} rating={jokes[0].rating} />
-        <Joke joke={jokes[1].joke} rating={jokes[1].rating} /> */}
 
         {jokesComponents}
 
