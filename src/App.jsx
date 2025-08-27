baac4c4f1cf9f4a1bb6b6c46cad99d0ea23013d6
manikantareddy268 <manikantareddy130263@gmail.com>
2025-08-28 02:06:10 +0530

    rendering from an array

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import Joke from './Joke'
 
 function App() {
 
-  const dadJoke2 ={
-    joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
-    rating: 4
-  }
+  const jokes =[
+    {
+      joke: "I'm reading a book about anti-gravity. It's impossible to put down!",
+      rating: 1  
+    },
+    {
+      joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
+      rating: 4
+    },
+    {
+      joke: "Why don't scientists trust atoms? Because they make up everything!",
+      rating: 5
+    },
+    {
+      joke: "Why did the bicycle fall over? Because it was two-tired!",
+      rating: 4
+    }
+  ]
   return (
       <div>
         <h1>Dad Jokes</h1>
 
-        <Joke joke="I'm reading a book about anti-gravity. It's impossible to put down!" rating={3} />
-        <Joke joke={dadJoke2.joke} rating={dadJoke2.rating} />
-        <Joke {...dadJoke2} />
+        <Joke joke={jokes[0].joke} rating={jokes[0].rating} />
+        <Joke joke={jokes[1].joke} rating={jokes[1].rating} />
       </div>
   )
 }
