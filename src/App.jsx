96d8c827fe3a299ed91954fb71a34f1657d06ea9
manikantareddy268 <manikantareddy130263@gmail.com>
2025-09-13 01:18:52 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,62 @@
 import './App.css'
-import Joke from './Joke'
-import Form from './Form'
-import { useState } from 'react'
-import JokeForm from './JokeForm'
+import { useEffect, useState } from 'react'
+import ThisIsFine from './memes/this_is_fine.jpg'
+import LoveYourself from './memes/love_yourself.jpg'
+import Code from './memes/code.jpg'
+
+
 
 function App() {
+  const [images, setImages] = useState([])
+  const [title, setTitle] = useState("")
 
-  const [favorite, setFavorite] = useState(1)
-  const [showForm, setShowForm] = useState(false)
+  useEffect(() => {
+    console.log("after rendered")
+    document.title = title
 
-  const [jokes, setJokes] = useState([
-    {
-      id: 1,
-      // joke:
-      text: "I'm reading a book about anti-gravity. It's impossible to put down!",
-      // rating: 1  
-      likes: 0
-    },
-    {
-      id: 2,
-      // joke:
-      text: "Why did the scarecrow win an award? Because he was outstanding in his field!",
-      // rating: 4
-      likes: 0
-    },
-    // {
-    //   id: 3,
-    //   // joke:
-    //   text: "Why don't scientists trust atoms? Because they make up everything!",
-    //   // rating: 5
-    // },
-    // {
-    //   id: 4,
-    //   // joke:
-    //   text: "Why did the bicycle fall over? Because it was two-tired!",
-    //   // rating: 4
-    // }
-  ])
+    window.scroll({
+      top: Number.MAX_SAFE_INTEGER,
+      behavior: 'smooth' 
+    })
+  })
 
-  function handleClick() {
-    console.log("Button clicked!")          
-  }
-
-  function handleSubmit(e) {
-    console.log("Form submitted!")
-  }
-
-  const handleFavorite = (id) => {
-    setFavorite(id)
-  }
-
-  const handleNewJoke = (text) => {
-    const joke = {
-      text,
+  const handleAddImage = (src) => {
+    setImages([...images, {
       id: self.crypto.randomUUID(),
-      likes: 0
-    }
-    // jokes.push(joke) // This won't actually update the state or re-render the component
-    setJokes([joke, ...jokes])
-    setShowForm(false)
+      src
+    }])
   }
 
-  const handleDeleteJoke = (id) => {
-    setJokes(jokes.filter(joke => joke.id !== id))
-    console.log("Delete joke with id: ", id) 
-  }
-
-  const handleLike = (id) => {
-    
-    setJokes(jokes.map(joke => {
-      if (joke.id === id) {
-        return {
-          ...joke,
-          likes: joke.likes + 1
-        }
-      }
-      return joke
-    }))
-  }
-
-  const handleDislike = (id) => {
-    setJokes(jokes.map(joke => {
-      if (joke.id === id) {
-        return {
-          ...joke,
-          likes: joke.likes - 1
-        }
-      }
-      return joke
-    }))
-  }
-
-  const handleSort = () => {
-    setJokes([...jokes].sort((a, b) => b.likes - a.likes))
-  }
-
-  const handleAddNewJoke = () => {
-    setShowForm(true)
+  const handleDeleteImage = (id) => {
+    setImages(images.filter(image => image.id !== id))
   }
 
   return (
-      <div className="App">
-        <h1>Dad Jokes</h1>
+    <div className="App">
 
-        <button onClick={handleAddNewJoke}>Add a new joke</button> <br /> 
+      <h1>{title}</h1>
 
-        {showForm ? 
-          <JokeForm onNewJoke={handleNewJoke} /> 
-          : 
-          <>
-            <button onClick={handleSort}>Sort</button>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
 
-            {jokes.map(joke => (
-              <Joke 
-              onDelete={handleDeleteJoke} 
-              onFavorite={handleFavorite} favorite={favorite === joke.id} 
-              key={joke.id} 
-              onLike={handleLike}
-              onDislike={handleDislike}
-              {...joke} />
-            ))}
-          </>
-        }
+      {images.map(({id, src}) => (
+        <img onClick={() => handleDeleteImage(id)} key={id} src={src} alt="random" />
+      ))}
+
+      <div className="buttons">
+        <button onClick={() => handleAddImage(ThisIsFine)}>
+          This is fine
+        </button>
+
+        <button onClick={() => handleAddImage(LoveYourself)}>
+          Love yourself
+        </button>
+
+        <button onClick={() => handleAddImage(Code)}>
+          Code
+        </button>
       </div>
+
+    </div>
   )
 }
 
