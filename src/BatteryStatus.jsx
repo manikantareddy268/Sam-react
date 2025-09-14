2e48f797aca86b0a007693ad2cce6a56d6520883
manikantareddy268 <manikantareddy130263@gmail.com>
2025-09-14 16:54:42 +0530

    Event Listners && async useEffect

--- src/BatteryStatus.jsx
+++ src/BatteryStatus.jsx
@@ -1,18 +1,37 @@
  import { useEffect, useState } from 'react'
 
+//  navigator.getBattery = () => Promise.reject() // to test error handling
+
  export default function BatteryStatus() {
     const [isCharging, setIsCharging] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
+        let cleanup
+        let cancled = false
         navigator.getBattery().then(battery => {
-            setIsCharging(battery.charging)
+            if(cancled) return
+            const onBatteryChange = () => setIsCharging(battery.charging)
+            onBatteryChange() 
+            battery.addEventListener('chargingchange', onBatteryChange)
+            cleanup = () => battery.removeEventListener('chargingchange', onBatteryChange)
+        }).catch(err => {
+            if(cancled) return
+            console.error(err)
+            setError(true)
         })
+
+        return () => {
+            //cleanup
+            cancled = true
+            cleanup?.()
+        }
     }, [])
 
     return (
         <>
             <h2>Battery Status</h2>
-            <p>Charging : {isCharging? "Yes" : "No"}</p>
+            {error ? <p>Could not get the battery status</p> : <p>Battery is {isCharging? "charging" : "not charging"}</p>}
         </>
     )
  }
